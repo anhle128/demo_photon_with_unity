94730b84c445291104d5a5150dcb62f71dad34ab
Anh Le <anhle12892@gmail.com>
2015-11-30 15:28:16 +0700

    first create server

---
+++ server/StartCollectorServer/StartCollectorServer/Peer/PhotonAckPeer.cs
@@ -0,0 +1,32 @@
+﻿using Photon.SocketServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PhotonHostRuntimeInterfaces;
+namespace StartCollectorServer.Peer
+{
+    public class PhotonAckPeer : PeerBase
+    {
+        private IRpcProtocol rpcProtocol;
+        private IPhotonPeer photonPeer;
+
+
+        public PhotonAckPeer(IRpcProtocol protocol, IPhotonPeer unmanagedPeer)
+            : base(protocol, unmanagedPeer)
+        {
+            
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            // send an "ack" to client
+            OperationResponse response = new OperationResponse((byte)PhotonAckResponseTypes.Ack);
+            this.SendOperationResponse(response, sendParameters);
+        }
+    }
+}
---
+++ server/StartCollectorServer/StartCollectorServer/StarCollectorServer.cs
@@ -0,0 +1,27 @@
+﻿using Photon.SocketServer;
+using StartCollectorServer.Peer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StartCollectorServer
+{
+    public class StarCollectorServer : ApplicationBase
+    {
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new PhotonAckPeer(initRequest.Protocol, initRequest.PhotonPeer);
+        }
+
+        protected override void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void TearDown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
